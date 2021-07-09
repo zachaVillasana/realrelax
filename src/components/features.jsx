a29b65260bcc387d988fa6b99321f76795a81359
zachaVillasana <villasanazacha@gmail.com>
2021-07-09 18:56:31 -0400

    FINAL

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,37 +1,76 @@
 export const Features = (props) => {
   return (
     <div id='features' className='text-center'>
-      <div className='container'>
-
-        <div className='row'>
-          <div className='section-title'>
-            <h2>Inicia sesión o crea una cuenta</h2>
-          </div>
-          <form name='sentMessage' validate>
-            <div className='row email'>
-
-              <p>Dirección de e-mail:</p>
-              <div className='form-group correo'>
-                <input
-                  type='email'
-                  id='email'
-                  name='email'
-                  className='form-control'
-                  placeholder='Correo'
-                  required
-                />
-                <p className='help-block text-danger'></p>
+        <div className='section-title space'>
+                <h2>Registrate o Inicia Sesión</h2>
 
               </div>
+      <div className='container row'>
+
+      <form className="borde-form">
+                <h3>Registrarse</h3>
+
+            <div className="row">
+            <div className="form-group">
+                    <label>Nombre</label>
+                    <input type="text" className="form-control" placeholder="Nombre" />
+                </div>
+
+                <div className="form-group">
+                    <label>Apellido</label>
+                    <input type="text" className="form-control" placeholder="Apellido" />
+                </div>
+
             </div>
 
-            <div id='success'></div>
-            <button type='submit' className='btn btn-custom btn-lg'>
-              Continuar con e-mail
-            </button>
-          </form>
+            <div className="row">
+            <div className="form-group">
+                    <label>RUN</label>
+                    <input type="text" className="form-control" placeholder="xx.xxx.xxx-x" />
+                </div>
 
-        </div>
+                <div className="form-group">
+                    <label>Telefono</label>
+                    <input type="text" className="form-control" placeholder="+xx x xxxx xxxx" />
+                </div>
+
+            </div>
+            
+
+
+
+                <div className="form-group">
+                    <label>Correo</label>
+                    <input type="email" className="form-control" placeholder="Enter Correo" />
+                </div>
+
+                <div className="form-group">
+                    <label>Contraseña</label>
+                    <input type="password" className="form-control" placeholder="Contraseña" />
+                </div>
+
+                <button type="submit" className="btn btn-custom btn-lg btn btn-dark btn-lg btn-block">Registrarse</button>
+              
+            </form>
+
+
+            <form className="borde-form">
+                <h3>Inicia Seción</h3>
+
+                <div className="form-group">
+                    <label>Correo</label>
+                    <input type="email" className="form-control" placeholder="Correo" />
+                </div>
+
+        
+                <div className="form-group">
+                    <label>Contraseña</label>
+                    <input type="password" className="form-control" placeholder="Contraseña" />
+                </div>
+
+                <button type="submit" className=" btn btn-custom btn-lg btn btn-dark btn-lg btn-block">Inicia Sesión</button>
+            
+            </form>
 
       </div>
 
